7bab44aa3f127c938f99187aee2d294848d9d18a
Satyam Vivek <1906131@kiit.ac.in>
2023-02-27 15:37:33 +0530

    'code'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let bodyParser=require('body-parser');
 const encoder=bodyParser.urlencoded();
 let dotenv=require('dotenv');
 dotenv.config();
-const path=require('path');
+//const path=require('path');
 let port=process.env.PORT||9100;
 let mysql=require('mysql2');
 
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname+'/public'))
 
 app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+//app.set('views',path.join(__dirname,'views'));
 
 const con = mysql.createConnection({
   host: 'souliot.mariadb.database.azure.com',
